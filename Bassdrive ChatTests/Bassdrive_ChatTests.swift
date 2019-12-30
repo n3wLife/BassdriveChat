68dc91b0cc4830b1513a980c104bb11689b80f15
n3wLife <59354902+n3wLife@users.noreply.github.com>
2019-12-29 22:50:27 -0800

    Add files via upload

---
+++ Bassdrive ChatTests/Bassdrive_ChatTests.swift
@@ -0,0 +1,34 @@
+//
+//  Bassdrive_ChatTests.swift
+//  Bassdrive ChatTests
+//
+//  Created by Bryan Henry on 12/27/19.
+//  Copyright © 2019 n3wLife. All rights reserved.
+//
+
+import XCTest
+@testable import Bassdrive_Chat
+
+class Bassdrive_ChatTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
